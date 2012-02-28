ea25b178904369b90b3b257b039320598cc31e13
Zanterian <zanterian@gmail.com>
2012-02-28 02:26:11 -0800

    Fixed Leap year bug

--- dummydata/dates.pl
+++ dummydata/dates.pl
@@ -37,7 +37,7 @@ sub main{
 			if($month==1 || $month==3 || $month==5 || $month==7 || $month==8 || $month==10 || $month==12){
 				&full_month($year,$month);
 			}
-			elsif($month==2 || $month==4 || $month==6 || $month==9 || $month==11){
+			elsif($month==4 || $month==6 || $month==9 || $month==11){
 				&thirty_month($year,$month);
 			}
 			elsif($month==2){
