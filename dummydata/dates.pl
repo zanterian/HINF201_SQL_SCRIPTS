909ad5e90de4dbf873f36b5d2c4ec8c2088f837f
Zanterian <zanterian@gmail.com>
2012-02-28 01:54:43 -0800

    Updated the dates loops, setup the main instance variables

--- dummydata/dates.pl
+++ dummydata/dates.pl
@@ -1,18 +1,40 @@
 #!/usr/bin/perl
 sub full_month{
-
+	my ($year, $month);
+	$year = @_[0];
+	$month = @_[1];
+	for($day=1;$day<=31;$day++){
+		
+	}
 }
 sub thirty_month{
-	
+	my ($year, $month);
+	$year = @_[0];
+	$month = @_[1];
+	for($day=1;$day<=30;$day++){
+		
+	}
 }
 sub feb{
-
+	my ($year, $month);
+	$year = @_[0];
+	$month = @_[1];
+	for($day=1;$day<=28;$day++){
+		
+	}
 }
 sub leap_year{
-	
+	my ($year, $month);
+	$year = @_[0];
+	$month = @_[1];
+	for($day=1;$day<=29;$day++){
+		
+	}
 }
 
 sub main{
+	my @insert_statements, $SQL_string;
+
 	for($year=1972;$year<2015;$year++){
 		for($month=1;$month<=12;$month++){
 			if($month==1 || $month==3 || $month==5 || $month==7 || $month==8 || $month==10 || $month==12){
