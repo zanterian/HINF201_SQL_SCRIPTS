32d2c51cbe96a48ff060520460b6cb3f3683d347
Zanterian <zanterian@gmail.com>
2012-03-03 23:49:19 -0800

    Adding the trigger

--- Triggers.sql
+++ Triggers.sql
@@ -1,14 +1,35 @@
-/* Trigger Creation */
-CREATE OR REPLACE TRIGGER Doctor_Date_Check
-BEFORE INSERT ON Doctor
-DECLARE 
-	l_count NUMBER;
+/* Procedure to be used to add a time to the table Time -- this makes it easier */
+CREATE OR REPLACE PROCEDURE Insert_Into_Time (date_in IN DATE) IS
+BEGIN
+	INSERT INTO TIME (t) VALUES (date_in);
+END;
+/
+CREATE OR REPLACE FUNCTION Check_Time_Table (date_in IN DATE) RETURN NUMBER 
+IS
+	count_t NUMBER;
 BEGIN
 	SELECT count(*)
-	INTO l_count
+	INTO count_t
 	FROM Time
-	WHERE TIME.t = :new.d_employed_date
-	IF l_count = 0 THEN
-		INSERT INTO TIME (t) VALUES (:new.d_employed_date);
-	END IF
+	WHERE Time.t = date_in;
+	RETURN count_t;
 END;
+
+/* Doctor Table Trigger */
+CREATE OR REPLACE TRIGGER Doctor_Time_Trigger
+BEFORE INSERT ON Doctor
+FOR EACH ROW
+DECLARE
+	date_check NUMBER;
+	new_date DATE;
+BEGIN
+	date_check := Check_Time_Table(:new.d_employed_date);
+	IF date_check = 0 THEN
+		Insert_Into_Time(:new.d_employed_date);	
+	END IF;
+
+END;
+/
+/* Patient BD Trigger  */
+CREATE OR REPLACE TRIGGER Patient_Additional_Time_Trigger
+BEFORE INSERT ON Patient_Additional
