ca3187a8cfaac540cd2cdd7388e96482d01f363f
Zanterian <zanterian@gmail.com>
2012-03-04 01:34:00 -0800

    Finished writing triggers

--- Triggers.sql
+++ Triggers.sql
@@ -1,4 +1,4 @@
-/* Procedure to be used to add a time to the table Time -- this makes it easier */
+/* Procedure to be used to add a time to the table Time - this makes it easier*/
 CREATE OR REPLACE PROCEDURE Insert_Into_Time (date_in IN DATE) IS
 BEGIN
 	INSERT INTO TIME (t) VALUES (date_in);
@@ -14,14 +14,13 @@ BEGIN
 	WHERE Time.t = date_in;
 	RETURN count_t;
 END;
-
+/********************************************************************/
 /* Doctor Table Trigger */
 CREATE OR REPLACE TRIGGER Doctor_Time_Trigger
 BEFORE INSERT ON Doctor
 FOR EACH ROW
 DECLARE
 	date_check NUMBER;
-	new_date DATE;
 BEGIN
 	date_check := Check_Time_Table(:new.d_employed_date);
 	IF date_check = 0 THEN
@@ -30,6 +29,30 @@ BEGIN
 
 END;
 /
+
 /* Patient BD Trigger  */
 CREATE OR REPLACE TRIGGER Patient_Additional_Time_Trigger
 BEFORE INSERT ON Patient_Additional
+FOR EACH ROW
+DECLARE 
+	date_check NUMBER;
+BEGIN
+	date_check := Check_Time_Table(:new.pa_bd);
+	IF date_check = 0 THENN
+		Insert_Into_Time(:new.pa_bd);
+	END IF;
+END;
+/
+/* Patient Visits */
+CREATE OR REPLACE TRIGGER Patient_Visit_Trigger
+BEFORE INSERT ON Patient_Visit
+FOR EACH ROW
+DECLARE
+	date_check NUMBER;
+BEGIN
+	date_check := Check_Time_Table(:new.pv_t);
+	IF date_check = 0 THEN
+		Insert_Into_Time(:new.d_employed_date);
+	END IF;
+END;
+/
