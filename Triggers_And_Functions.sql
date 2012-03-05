6280de2656095f7f4a07995d75917af26976e575
Zanterian <zanterian@gmail.com>
2012-03-05 12:48:21 -0800

    Adding Trigger Cleanup, begin debugging

--- Triggers_And_Functions.sql
+++ Triggers_And_Functions.sql
@@ -16,7 +16,7 @@ BEGIN
 	RETURN count_t;
 END;
 -- Boolean-like function -- 1 for True, 0 for false
-CREATE OR REPLACE FUNCTION Check_If_Patient_Visit_Possible (date_in IN DATE, doctor_id IN CHAR(4)) RETURN NUMBER
+CREATE OR REPLACE FUNCTION Check_If_Visit_Possible (date_in IN DATE, doctor_id IN CHAR(4)) RETURN NUMBER
 IS
 	doctor_employed DATE;
 BEGIN
@@ -77,7 +77,7 @@ DECLARE
 	appointment_check NUMBER;
 BEGIN
 	date_check := Check_Time_Table(:new.pv_t);
-	appointment_check := Check_If_Patient_Visit_Possible (:new.pv_t,:new.pv_d_id);
+	appointment_check := Check_If_Visit_Possible (:new.pv_t,:new.pv_d_id);
 	IF appointment_check = 0 THEN 
 		raise_application_error(-20001,'Appointment Cannot Be Before Doctor Employed Date');
 	END IF;
