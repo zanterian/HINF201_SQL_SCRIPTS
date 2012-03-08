9315ebc77e4f165a07170999038b84eb87806dab
Zanterian <zanterian@gmail.com>
2012-03-07 16:29:55 -0800

    Fixed Cleanup functions

--- Cleanup.sql
+++ Cleanup.sql
@@ -1,3 +1,10 @@
+/* Drop Procedures,Functions,Triggers */
+DROP TRIGGER Patient_Visit_Trigger;
+DROP TRIGGER Pat_Addi_Time_Trigger;
+DROP TRIGGER Doctor_Time_Trigger;
+DROP FUNCTION Check_If_Visit_Possible;
+DROP FUNCTION Check_Time_Table;
+DROP PROCEDURE Insert_Into_Time;
 /*Drop Events*/
 DROP TABLE Patient_Visit;
 DROP TABLE Encounter;
--- Clear_Triggers_And_Functions.sql
+++
@@ -1,6 +0,0 @@
-DROP PROCEDURE Insert_Into_Time;
-DROP FUNCTION Check_Time_Table;
-DROP FUNCTION Check_If_Visit_Possible;
-DROP TRIGGER Doctor_Time_Trigger;
-DROP TRIGGER Patient_Additional_Time_Trigger;
-DROP TRIGGER Patenht_Visit_Trigger;
