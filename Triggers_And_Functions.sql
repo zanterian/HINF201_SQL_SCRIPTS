0c03784eaab8afeeefeed6cbda5f4160ca1c6987
Zanterian <zanterian@gmail.com>
2012-03-07 16:03:25 -0800

    Thanks to Grant, this works well.

--- Triggers_And_Functions.sql
+++ Triggers_And_Functions.sql
@@ -17,18 +17,16 @@ BEGIN
 END;
 /
 -- Boolean-like function -- 1 for True, 0 for false
-CREATE OR REPLACE FUNCTION Check_If_Visit_Possible (date_in IN DATE, doctor_id IN CHAR(4)) RETURN NUMBER
+CREATE OR REPLACE FUNCTION Check_If_Visit_Possible (date_in IN DATE, doctor_id IN CHAR) RETURN NUMBER
 IS
 	return_value NUMBER;
-
-	DATE docto_employed 
-AS
-	SELECT d_employed_date
-	FROM Doctor
-	WHERE Doctor.d_id = doctor_id;
+	doctor_employed DATE;
 
 BEGIN
-	
+	SELECT d_employed_date
+	INTO doctor_employed
+	FROM Doctor
+	WHERE Doctor.d_id = doctor_id;
 	IF doctor_employed < date_in  THEN
 		RETURN 1;
 	END IF;
@@ -80,11 +78,11 @@ DECLARE
 	date_check NUMBER;
 	appointment_check NUMBER;
 BEGIN
-	date_check := Check_Time_Table(:new.pv_t);
 	appointment_check := Check_If_Visit_Possible (:new.pv_t,:new.pv_d_id);
 	IF appointment_check = 0 THEN 
 		raise_application_error(-20001,'Appointment Cannot Be Before Doctor Employed Date');
 	END IF;
+	date_check := Check_Time_Table(:new.pv_t);
 	IF date_check = 0 THEN
 		Insert_Into_Time(:new.pv_t);
 	END IF;
